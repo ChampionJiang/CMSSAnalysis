6468f8c3356325b1759dcbd284e5e336e6053434
unknown <qjiang@CHN-QJIANG1.corp.microstrategy.com>
2015-08-24 17:53:49 +0800

    add CSV lib

--- src/com/Import/CSVImport.java
+++ src/com/Import/CSVImport.java
@@ -1,16 +1,28 @@
 package com.Import;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import com.connector.Connector;
 import com.jsp.smart.File;
+import com.opencsv.CSVReader;
 import com.storage.MyTable;
 import com.storage.ObjectAlreadyInitializedException;
 
 public class CSVImport implements Connector {
 	
-	public CSVImport(File file) {
-		System.out.println("csv file is not supported now");
+	public CSVImport(File file) throws IOException {
+		CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()));
+		// if the first line is the header
+		String[] header = reader.readNext();
+
+		// iterate over reader.readNext until it returns null
+		String[] line = reader.readNext();
+		
+		
+		
 	}
 
 	@Override
